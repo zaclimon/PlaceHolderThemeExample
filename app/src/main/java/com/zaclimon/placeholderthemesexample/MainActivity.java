34bba27a4ee615cb521b1d94b66655bcf07c02c5
zaclimon <isaacpateau05@gmail.com>
2017-04-09 10:02:45 -0400

    Adapt the UI a little bit

--- app/src/main/java/com/zaclimon/placeholderthemesexample/MainActivity.java
+++ app/src/main/java/com/zaclimon/placeholderthemesexample/MainActivity.java
@@ -1,13 +1,21 @@
 package com.zaclimon.placeholderthemesexample;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.fragmentPlaceholder, MainFragment.newInstance(null, null));
+        fragmentTransaction.commit();
     }
 }
